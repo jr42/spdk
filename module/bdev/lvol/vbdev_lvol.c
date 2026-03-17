5c01474e94f0d4c6db01ac1fa69cec73e9c6a251
Abhilash Shetty <abhilash.shetty@datacore.com>
2026-03-17 10:27:00 +0000

    new debug
    Signed-off-by: Abhilash Shetty <abhilash.shetty@datacore.com>

--- module/bdev/lvol/vbdev_lvol.c
+++ module/bdev/lvol/vbdev_lvol.c
@@ -153,30 +153,29 @@ vbdev_lvs_set_timeout(struct spdk_lvol_store *lvs, uint64_t timeout_in_sec,
 	return 0;
 }
 
-static void
+static void 
 lvs_bs_dev_reset_cb(struct spdk_bdev_io *bdev_io,
-		    bool success,
-		    void *cb_arg)
+        bool success,
+        void *cb_arg)
 {
 	struct lvstore_reset_cb_args *ctx = cb_arg;
 	if (ctx->cb_fn) {
 		ctx->cb_fn(ctx->lvs, success, ctx->cb_arg);
 	}
 	spdk_bdev_free_io(bdev_io);
-
 	free(ctx);
 }
 
 int
 vbdev_lvs_bs_bdev_reset(struct spdk_lvol_store *lvs,
-			spdk_lvs_reset_completion_cb cb_fn, void *cb_arg)
+			      spdk_lvs_reset_completion_cb cb_fn, void *cb_arg)
 {
 	struct spdk_bs_dev *bs_dev = lvs->bs_dev;
 	struct lvstore_reset_cb_args *args;
 	SPDK_DEBUGLOG(bdev, "starting reset new change");
 	args = calloc(1, sizeof(*args));
 	if (!args) {
-		return -ENOMEM;
+    	return -ENOMEM;
 	}
 
 	args->cb_arg = cb_arg;
